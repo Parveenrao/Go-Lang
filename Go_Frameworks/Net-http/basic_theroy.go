90086645798b169cc6d59940d635968aa73d313f
Parveen Yadav <parveeenrao7727@gmail.com>
2026-02-13 10:43:36 +0530

    http server

---
+++ Go_Frameworks/Net-http/basic_theroy.go
@@ -0,0 +1,13 @@
+package main
+
+/* net\http is Go standard library for building web server and clients
+
+   => Every go http server is based on
+     
+      func(w http.ResponseWriter , r*http request) 
+	  
+	  this is called handler 
+	  w --> what you send back (response)
+	  r --> what client sent (request) */
+
+	  
