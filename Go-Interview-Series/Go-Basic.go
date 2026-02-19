e985acb0facd626a2c9a7f9536a19111d05d0a49
Parveen Yadav <parveeenrao7727@gmail.com>
2026-02-19 20:55:33 +0530

    Interview

---
+++ Go-Interview-Series/Go-Basic.go
@@ -0,0 +1,36 @@
+package gointerviewseries
+
+/*  
+
+  1 . Go is statically typed language 
+      -> Means Type is checked at compile time 
+	  -> Errors are caught before running 
+
+  2. Go is compilled language 
+     -> Go source code (.go file) is directly converted into native machine code for your operating system
+
+      .go source code
+            ↓
+      Go compiler (gc)
+            ↓
+      Machine code
+            ↓
+      Single binary executable
+
+  3. Go is not traditional object-oriented  language , but it support some concepts like abstraction 
+     -> Encapsulation (via structs)
+	 -> Polymorphism  (via interface)
+	 -> Abstraction (via interface)	  
+
+  4. Go is procedural language 
+     -> Program flow step by step 
+	 -> Logic is written using functions 
+	 -> You explicitly control execution 
+	 
+	 
+	 But Go has some elements of function 
+	 -> First calls function 
+	 -> Anonymous function 
+	 -> Closure functions
+
+*/	 
